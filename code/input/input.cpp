d744e5f20cc0fd00669271afca098030535c3229
truongcntn2017 <1712195@student.hcmus.edu.vn>
2023-07-30 14:46:46 +0700

    add week 1

---
+++ code/input/input.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int year = 0;
+    int age = 0;
+    
+    string name = " ";
+    //print a message to the user <- this line is a comment
+    cout<<"Input a year: ";
+    //get the user response and assign it to the year<- this line is a comment
+    cin >> year;
+    cout<<"This year is "<<year<<endl;
+
+    cout << "Current age is "<<age<<endl;
+
+    cout << "Input your age: ";
+    cin >> age;
+    cout<<"Your age is "<<age<<endl;
+
+    return 0;
+
+}
---
+++ code/type/type.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+ {
+     cout<<"int size = "<<sizeof(int)<<"\n";
+     cout<<"int max = "<<INT32_MAX<<"\n";
+     cout<<"int min = "<<INT32_MIN<<"\n";  
+     cout<<"long long size = "<<sizeof(long long)<<"\n";
+     cout<<"long long max = "<<INT64_MAX<<"\n";
+     cout<<"long long min = "<<INT64_MIN<<"\n";
+     return 0;
+ }
